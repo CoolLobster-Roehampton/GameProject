3297f016bf4b38b3ba48341eb4283a46779d8c42
CoolLobster-Rohampton <kazank@roehampton.ac.uk>
2026-04-17 12:16:18 +0100

    Added collision to teleport

--- Assets/TimeTravel.cs
+++ Assets/TimeTravel.cs
@@ -3,22 +3,65 @@ using UnityEngine;
 public class TimeTravel : MonoBehaviour {
     public GameObject Player;
     public Vector3 TeleportOffset;
+    public LayerMask teleportBlockerMask;
     bool isOffset = false;
-    CharacterController cc; 
+
+    CharacterController capsule;
+    float capsuleHeight, capsuleRadius;
+    Vector3 capsuleCenter;
 
     void Start() {
-        cc = Player.GetComponent<CharacterController>();
+        capsule = Player.GetComponent<CharacterController>();
+        capsuleHeight = capsule.height * Player.transform.lossyScale.y;
+        capsuleRadius = capsule.radius * Mathf.Max(Player.transform.lossyScale.x, Player.transform.lossyScale.z);
+        capsuleCenter = capsule.center;
     }
+
+    bool CanTeleport(Vector3 targetPosition) {
+        Vector3 worldCenter =
+            targetPosition +
+            Player.transform.rotation *
+            Vector3.Scale(capsule.center, Player.transform.lossyScale);
+
+        float halfHeight = Mathf.Max(
+            capsuleHeight / 2f - capsuleRadius,
+            0.01f
+        );
+
+        Vector3 bottom = worldCenter + Vector3.down * halfHeight;
+        Vector3 top    = worldCenter + Vector3.up   * halfHeight;
+
+        Collider[] hits = Physics.OverlapCapsule(
+            bottom,
+            top,
+            capsuleRadius,
+            teleportBlockerMask,
+            QueryTriggerInteraction.Ignore
+        );
+
+        foreach (var hit in hits)
+        {
+            if (hit == capsule) continue; // ignore self
+            return false;
+        }
+
+        return true;
+    }
+
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
-            Vector3 newPos = Player.transform.position + isOffset ? -TeleportOffset : TeleportOffset;
+            Vector3 newPos = Player.transform.position + (isOffset ? -TeleportOffset : TeleportOffset);
 
-            cc.enabled = false;
-            Player.transform.position = newPos;
-            cc.enabled = true;
-            
-            isOffset = !isOffset;
+            if (CanTeleport(newPos)) {
+                capsule.enabled = false;
+                Player.transform.position = newPos;
+                capsule.enabled = true;
+
+                isOffset = !isOffset;
+            }
+
+
         }
     }
 }
