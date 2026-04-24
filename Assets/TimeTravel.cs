70ad0042f7eb0c06a8aa2b56a40178eef555e47c
blizzargame-oss <blizzargame@gmail.com>
2026-04-24 12:58:53 +0100

    tELEPORT SOLVE so far

--- Assets/TimeTravel.cs
+++ Assets/TimeTravel.cs
@@ -23,11 +23,16 @@ public class TimeTravel : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.E)) {
             //Vector3 newPos = Player.transform.position + isOffset ? -TeleportOffset : TeleportOffset;
 
-            cc.enabled = false;
+            //cc.enabled = false;
             //Player.transform.position = newPos;
-            cc.enabled = true;
+            //cc.enabled = true;
             
             isOffset = !isOffset;
         }
+
+    }
+
+    public void SetTeleportAllowed(bool state) {
+        teleportAllowed = state;
     }
 }
